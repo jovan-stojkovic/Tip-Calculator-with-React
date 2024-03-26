2fc91822f8e9c23d391793d8b194696adcf25869
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-03-26 04:57:05 +0100

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./Styles.scss";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const buttons = [5, 10, 15, 25, 50, "Custom"];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main>
+      <div className="container">
+        <div className="logo-container"></div>
+        <div className="app">
+          <div className="left">
+            <div className="bill-section">
+              <div className="labels">
+                <p className="label-p">Bill</p>
+                <p className="error">Please Type the Bill</p>
+              </div>
+              <input type="number" placeholder="0" />
+            </div>
 
-export default App
+            <div className="tip-section">
+              <label htmlFor="tips">Select Tip %</label>
+              <div className="grid">
+                {buttons.map((button) => {
+                  return (
+                    <button
+                      id={button}
+                      className="button"
+                      value={button}
+                      key={button}
+                    >
+                      {button}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="people-section">
+              <div className="labels">
+                <p className="label-p">Number of People</p>
+                <p className="error">Can't be zero</p>
+              </div>
+              <input type="number" placeholder="0" />
+            </div>
+          </div>
+
+          <div className="right">
+            <div className="amount-section">
+              <div className="text">
+                <h2>Tip Amount</h2>
+                <p>/ person</p>
+              </div>
+              <h1>$0.00</h1>
+            </div>
+
+            <div className="amount-section">
+              <div className="text">
+                <h2>Total</h2>
+                <p>/ person</p>
+              </div>
+              <h1>$0.00</h1>
+            </div>
+
+            <button className="reset">RESET</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
